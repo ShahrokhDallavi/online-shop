855018d8e69c45f31bd61ec133a3e470a95664b4
shahrokh <shahrokhdallavi@gmail.com>
2024-08-10 17:38:15 +0330

    fix style

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -10,7 +10,7 @@ const Loading = () => {
         <div className='flex justify-center items-center text-center'>
             <img src={login} alt='login banner' className='object-cover h-screen w-full'/>
             <img src={loginLogo} alt='login logo' className='absolute top-20 animate-pulse w-24'/>
-            <div className='absolute flex flex-col text-white bottom-0 px-4 py-6 bg-black/80 backdrop-blur rounded-t-3xl'>
+            <div className='fixed flex flex-col text-white bottom-0 px-4 py-6 bg-black/80 backdrop-blur rounded-t-3xl'>
                 <h5 className='font-bold text-2xl tracking-wider'>Start Finding Your Version The Best Fashion Style</h5>
                 <p className='text-sm my-4 leading-relaxed tracking-wider text-gray/50 mx-4'>Your appearance shows your quality so give your best for your best fashion</p>
                 <Link to='/products'>
