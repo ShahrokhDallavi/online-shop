d76857e767976da2cb19905c690c0256ebc35e63
shahrokh <shahrokhdallavi@gmail.com>
2024-08-10 17:33:15 +0330

    fix style

--- src/components/loginForm/loginBnner.js
+++ src/components/loginForm/loginBnner.js
@@ -10,7 +10,7 @@ const loginBnner = () => {
         <div className='flex justify-center items-center'>
             <img src={login} alt='login banner' className='object-cover h-screen w-full'/>
             <img src={loginLogo} alt='login logo' className='absolute top-20 animate-pulse'/>
-            <div className='absolute flex flex-col text-white bottom-44 px-5'>
+            <div className='absolute flex flex-col text-white bottom-40 px-5'>
                 <h5 className='mb-2 font-bold text-xl tracking-wider'>Welcome to your store</h5>
                 <p className='text-xs mb-4 tracking-wide leading-relaxed text-gray'>Discover Your Style: Shop Smarter, Live Better! Thank you for your trust and support!</p>
                 <Link to='/login'>
